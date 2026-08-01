f6a647208e07b969b56c49441dc3dab081acf01e
KitekXP <tomek.wojtczak@gmail.com>
2026-08-01 21:26:03 +0200

    Added logging

--- include/crab/log.hpp
+++ include/crab/log.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <cstdio>
+#include <cstdint>
 #include <iostream>
 #include <string>
 
@@ -17,39 +19,51 @@ namespace crab {
     private:
         FILE *log_out;
         int8_t verbosity;
-    
-    // Helper proxy for << stream operators
-	template <int8_t MinVerbosity>
-	struct LogStream {
-		log &logger;
-		const char *prefix;
 
-		template <typename T>
-		LogStream &operator<<(const T &msg) {
-			if (logger.verbosity > MinVerbosity) {
-				crab::FP2Str(logger.log_out) << prefix << msg;
-			}
-			return *this;
-		}
-	};
+        // Helper proxy for << stream operators
+        template <int8_t MinVerbosity>
+        struct LogStream {
+            log &logger;
+            const char *prefix;
+
+            LogStream(log &l, const char *p) : logger(l), prefix(p) {}
+
+            template <typename T>
+            LogStream &operator<<(const T &msg) {
+                if (logger.verbosity > MinVerbosity) {
+                    crab::FP2Str(logger.log_out) << prefix << msg;
+                }
+                return *this;
+            }
+
+            // support stream manipulators like std::endl
+            LogStream &operator<<(std::ostream& (*manip)(std::ostream&)) {
+                if (logger.verbosity > MinVerbosity) {
+                    crab::FP2Str(logger.log_out) << manip;
+                }
+                return *this;
+            }
+        };
 
     public:
         log() : log_out(stdout), verbosity(1) {}
+
         // Overload >> to set output file pointer
         log &operator>>(FILE *fptr) {
             log_out = fptr;
             return *this;
         }
 
-        // Note: Renamed from verbosity() to avoid colliding with the verbosity variable
-        void set_verbosity(const int8_t level) {
+        // Set verbosity level
+        log &set_verbosity(const int8_t level) {
             verbosity = level;
+            return *this;
         }
 
-        // Stream objects for each severity using your updated names
-        LogStream<0> inf{*this, CRAB_LOG_INFO};
-        LogStream<1> warn{*this, CRAB_LOG_WARN};
-        LogStream<-2> err{*this, CRAB_LOG_ERROR};
-        LogStream<2> dbg{*this, CRAB_LOG_DEBUG};
+        // Methods returning stream proxies for each severity
+        LogStream<0> inf() { return LogStream<0>(*this, CRAB_LOG_INFO); }
+        LogStream<1> warn() { return LogStream<1>(*this, CRAB_LOG_WARN); }
+        LogStream<-1> err() { return LogStream<-1>(*this, CRAB_LOG_ERROR); }
+        LogStream<2> dbg() { return LogStream<2>(*this, CRAB_LOG_DEBUG); }
     };
 }
